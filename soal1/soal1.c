354173975b3bad20a29cd129f0fbf55bc3288676
arifdarma <arifdarmaalthia@gmail.com>
2019-03-14 15:35:00 +0700

    soal nomor 1

---
+++ soal1/soal1.c
@@ -0,0 +1,61 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <dirent.h>
+
+int main() {
+	pid_t pid, sid;
+
+	pid = fork();
+
+	if (pid < 0) {
+   		exit(EXIT_FAILURE);
+  	}
+	if (pid > 0) {
+		exit(EXIT_SUCCESS);
+	}
+
+	umask(0);
+
+	sid = setsid();
+
+	if (sid < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	if ((chdir("/")) < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	close(STDIN_FILENO);
+  	close(STDOUT_FILENO);
+  	close(STDERR_FILENO);
+
+  	while(1) {
+		DIR* FD;
+		struct dirent* in_file;
+		char lama[100]={0};
+		char baru[100]={0};
+		char tujuan[100]={0};
+		FD=opendir(".");
+    		while(in_file=readdir(FD)){
+			strcpy(lama,in_file->d_name);
+			if(strchr(lama,'.')!=NULL && strcmp(".png",&lama[strlen(lama)-4])==0){
+				memset(baru,0,sizeof baru);
+				strncpy(baru,lama,strlen(lama)-4);
+				strcat(baru,"_grey.png");
+				strcpy(tujuan,"/home/arifdarma/modul2/gambar/");
+				strcat(tujuan,baru);
+				rename(lama,tujuan);
+			}
+		}
+    	sleep(10);
+  	}
+  	exit(EXIT_SUCCESS);
+}
