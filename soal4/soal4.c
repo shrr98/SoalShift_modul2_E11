be728bf37bb767c2318716a7900f609b9cde451d
trash <shinrezky@gmail.com>
2019-03-15 10:28:48 +0200

    soal4 daemon fix

--- soal4/soal4.c
+++ soal4/soal4.c
@@ -10,13 +10,14 @@
 #include <string.h>
 
 int main() {
-  int link[2];
   char directory[] = "/home/trash/Documents/makanan/";
   char file_enak[] = "/home/trash/Documents/makanan/makan_enak.txt";
 
-  if (pipe(link)==-1)
-    exit(EXIT_FAILURE);
   pid_t daemon, sid;
+
+    int link[2];
+    if (pipe(link)==-1)
+      exit(EXIT_FAILURE);
 
 	daemon = fork();
 
@@ -47,14 +48,15 @@ int main() {
   while(1){
     pid_t sleep_id = fork();
     if(sleep_id<0) exit(EXIT_FAILURE);
-    else if(sleep_id==0){
+    else if(sleep_id>0){
       pid_t pid = fork();
-      if (pid== -1)
+      if (pid<0)
         exit(EXIT_FAILURE);
       else if(pid==0){  // child
         close(link[0]);
         dup2(link[1], STDOUT_FILENO);
         close(link[1]);
+        close(link[0]);
         char *argv[] = {"find", file_enak, "-amin", "-0.5", NULL};
         execv("/usr/bin/find", argv);
       }
@@ -62,10 +64,10 @@ int main() {
         DIR *dir;
         struct dirent *file;
         close(link[1]);
-        char buffer[15], new_file[100];
+        char buffer[128]="\0", new_file[100];
         int count;
         int n_buff = read(link[0],buffer, sizeof(buffer));
-        close(link[0]);
+        
         if(n_buff>0){   // kalau pernah dibuka
           dir = opendir(directory);
           count = 0;
@@ -74,15 +76,15 @@ int main() {
               count++;
             }
           }
+          close(link[0]);
+          close(link[1]);
           sprintf(new_file, "%smakan_sehat%d.txt", directory, ++count);
           char *argv[] = {"touch", new_file, NULL};
           execv("/usr/bin/touch", argv);
         }
-        else exit(EXIT_SUCCESS);
       }
     }
     else{
-      wait(NULL);
       sleep(5);
     }
   }
